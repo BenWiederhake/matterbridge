2ef350898215586b95f2a3b932aa664c958eb3ca
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2022-03-08 19:12:24 +0100

    Discord: Crudely fix compilation by completely disabling message edits and file uploads

--- bridge/discord/handlers.go
+++ bridge/discord/handlers.go
@@ -56,7 +56,7 @@ func (b *Bdiscord) messageUpdate(s *discordgo.Session, m *discordgo.MessageUpdat
 		return
 	}
 	// only when message is actually edited
-	if m.Message.EditedTimestamp != "" {
+	if false { // FIXME: m.Message.EditedTimestamp != ""
 		b.Log.Debugf("Sending edit message")
 		m.Content += b.GetString("EditSuffix")
 		msg := &discordgo.MessageCreate{
